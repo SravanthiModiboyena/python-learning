005f8c41e32f60f1c76a8d7d29a8bdbe5bbd59a7
Modiboyena <modiboyena@gmail.com>
2026-01-14 22:29:13 +0530

    loops

---
+++ 00basics/12 basic.py
@@ -0,0 +1,46 @@
+a = 10
+b = -10
+
+if a >= b:
+   print("a is greater or equal to b.")
+if (a == 0) or (b == 0):
+   print("At least one is 0.")
+if (a == 0) and (b == 0):
+   print("Both are 0.")
+   
+print("Program ends")
+
+
+# area of square
+
+print("-------Area of Square-------")
+l= 3
+b= 3
+c= l * b
+print("Area of Square:",c)
+
+# Biggest of two integers
+
+print("-------biggest-------")
+a = 10
+b = 20
+if a>b:
+   print("a is big")
+else:
+    print("b is big")
+
+
+# Even or Odd
+
+print("------Even or odd------")
+
+a=15
+if a % 2 == 0:
+    print("a is even")
+else:
+    print("a is odd")
+    
+    
+
+
+
