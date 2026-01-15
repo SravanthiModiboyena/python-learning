5080e7895c91c180ccc9918c1244b748029f19fe
Modiboyena <modiboyena@gmail.com>
2026-01-15 20:27:38 +0530

    functions_returnValues

---
+++ 04functions/02functions_returnValues.py
@@ -0,0 +1,12 @@
+# calc_sum(*args) : We can pass unlimited number of arguments to a function using *args.
+# *args : *args is used to pass a variable number of non-keyworded arguments list and the function takes them as a tuple.
+
+# return : The return statement is used to exit a function and go back to the place from where it was called.
+
+def calc_sum(*args):
+    return sum(args)
+
+print(calc_sum(10, 20, 20))
+print(calc_sum(10, 10, 20))
+print(calc_sum(10, 1, 10, 4, 5))
+print(calc_sum(1, 1, 10, 4, 5, 1)) 
