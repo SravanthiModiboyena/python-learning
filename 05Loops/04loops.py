1942857b6844aebc209ca979a1c8d62f08fa89b8
Modiboyena <modiboyena@gmail.com>
2026-01-17 10:00:24 +0530

    Added code

---
+++ 05Loops/04loops.py
@@ -0,0 +1,15 @@
+# Search a number in the below tuple 
+tuple1 = (1, 2, 4, 7, 9, 12, 8, 13, 1, 32, 4, 7, 9, 13)
+
+i = 0
+while i < len(tuple1):
+    if tuple1[i] == 13:
+        print("Number 13 found at index ", i)
+    else:
+        print("Finding 13...")
+    i += 1
+
+ # if i<len(tuple) is true its comes to if clause and checks until the condition true it goes to else till end of the tuples
+ 
+#it checks complete len(tuple) 
+ 
