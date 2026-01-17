31b483adf5cccea32a792b2375fec1824035135d
Modiboyena <modiboyena@gmail.com>
2026-01-17 19:08:22 +0530

    class codes

---
+++ 07Oops/01classes.py
@@ -0,0 +1,16 @@
+# we need classes just to avoid repetition and organize code neatly.
+
+# Basic Structure of class : class ClassName: #variables, methods
+# object is an instance of a class 
+
+class Student:
+    name="Sam"
+    age=9
+s1=Student()
+print(s1.age)
+print(s1.name)
+
+# s1 is a object 
+# name, age attributes 
+# Student() constructor call 
+
